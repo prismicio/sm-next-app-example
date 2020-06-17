bc80e922edaf32f1a39b5958200eca37f12c06c1
Hugo Villain <hugo.villain@prismic.io>
2020-06-17 12:29:52 +0200

    Example theme override

--- utils/theme.js
+++ utils/theme.js
@@ -1,20 +1,26 @@
 import theme from 'essential-slices/src/theme'
 import { merge } from 'theme-ui'
 
+/**
+ * example override for theme props
+ */
+
+const container = {
+  ...theme.container,
+  slice: {
+    background: '#F7F7F7',
+  },
+  eyebrow: {
+    color: 'tomato',
+  },
+  item: {
+    background: '#FFF',
+    border: '1px solid #111',
+    padding: '12px',
+  },
+},
+
 export default merge(theme, {
   ...theme,
-  container: {
-    ...theme.container,
-    slice: {
-      background: '#F7F7F7',
-    },
-    eyebrow: {
-      color: 'tomato',
-    },
-    item: {
-      background: '#FFF',
-      border: '1px solid #111',
-      padding: '12px',
-    },
-  },
+  /** ...container */
 })
